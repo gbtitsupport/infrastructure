edc4f991fe2917a0792a6eca74e53e40f93a26cf
Samson <samsonispower@gmail.com>
2026-09-05 20:57:29 +0100

    Update workflows and move tf files into seperate environments

---
+++ prod-environment/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      Project     = "gbt cloud infrastructure"
+      ManagedBy   = "terraform"
+      Owner       = "pipeline-deployer"
+      Environment = "prod"
+    }
+  }
+}
---
+++ test-environment/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "gbt-infra-terraform-state-b7820b85"
+    key            = "test/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+}
