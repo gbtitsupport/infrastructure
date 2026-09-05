3fc850ce3b148b62f850321924e5787b58a114a1
Samson <samsonispower@gmail.com>
2026-09-05 21:46:26 +0100

    Update test state key

--- test-environment/backend.tf
+++ test-environment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "gbt-infra-terraform-state-b7820b85"
-    key            = "test/terraform.tfstate"
+    key            = "test.terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
